e00b377b64d7288373d3adec1f62b5286ec279ad
tomilodk <tomilobas@gmail.com>
2022-10-09 21:48:28 +0200

    Playermovement with rb

--- src/Svampe Jagt/Assets/SvampeJagt/Scripts/PlayerMovement.cs
+++ src/Svampe Jagt/Assets/SvampeJagt/Scripts/PlayerMovement.cs
@@ -42,12 +42,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement =
             transform.right * horizontal + transform.forward * vertical;
 
-<<<<<<< Updated upstream
-        transform.position += movement * movementSpeed * Time.deltaTime;
-
-=======
         rb.AddForce(movement * movementSpeed * 1000f * Time.deltaTime);
->>>>>>> Stashed changes
     }
 
     private void RotatePlayer()
