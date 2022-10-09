95aad0420aa733e232571e03fa35646d72692642
tomilodk <tomilobas@gmail.com>
2022-10-09 20:50:12 +0200

    Playermovement rotation applied

--- src/Svampe Jagt/Assets/SvampeJagt/Scripts/PlayerMovement.cs
+++ src/Svampe Jagt/Assets/SvampeJagt/Scripts/PlayerMovement.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
     private float movementSpeed = 6f;
+
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+
+    [SerializeField]
+    [
+        Header(
+            "The maximum angle the player can look up and or down (in degrees)")
+    ]
+    private float maxLookAngle = 90f;
+    public Transform head;
+
+
+    private float xRotation = 0f;
+
 
     void Start()
     {
@@ -12,9 +28,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        MovePlayer();
+        RotatePlayer();
     }
 
-    void FixedUpdate()
+    private void MovePlayer()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -22,5 +40,23 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
 
         transform.position += movement * movementSpeed * Time.deltaTime;
+    }
+
+    private void RotatePlayer()
+    {
+        float mouseX =
+            Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY =
+            Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -maxLookAngle, maxLookAngle);
+
+        head.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+
+
     }
 }
